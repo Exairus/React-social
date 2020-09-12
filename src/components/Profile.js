fedb16c280c65f6ffb69e8b0b0de68e8f54fa159
will <test@mail.com>
2020-09-12 18:24:01 +0300

    first commit of social network app

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+            <img src="https://kogdakotika.net/media/post_images/title_page_m_ChtRYfI.jpg" alt=""/>
+            </div>
+            <div>
+            ava + description
+            </div>
+            <div>
+            my post
+            <div>new post</div>
+            </div>
+            <div>
+            post 1
+            </div>
+            <div>
+            post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
